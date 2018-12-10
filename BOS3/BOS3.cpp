9ad0b7f6487a37d7b093b0840ca3eb7a17c3b375
kamtimur2014@yandex.ru <kamtimur2014@yandex.ru>
2018-12-10 18:41:43 +0300

    14 done

--- BOS3/BOS3.cpp
+++ BOS3/BOS3.cpp
@@ -9,24 +9,101 @@
 #include <openssl/pem.h>
 #include <openssl/md4.h>
 #include <openssl/des.h>
+#include <openssl/aes.h>
 #include <fstream>
 #include <string>
 #include <algorithm>
 #include <vector>
 #include <iterator> 
+#include <openssl/evp.h> 
+#include <sstream>
 using namespace std;
 
 char text[] = "C:\\Users\\t440s\\source\\repos\\BOS3\\10\\13\\13data.txt";
 string folder = "C:\\Users\\t440s\\source\\repos\\BOS3\\10\\13\\";
 string string_to_hex(const std::string& input);
 void hex2bin(const char* src, char* target);
-
-
+int task13();
+int AesDecrypt(const std::string &sAesKey, const std::string &ToDecrypt, std::string &Decrypted);
 
 char buffer[100];
 int main() 
 {
-	//setlocale(LC_ALL, "en_US.UTF-8");
+	OpenSSL_add_all_algorithms();
+	OpenSSL_add_all_ciphers();
+	OpenSSL_add_all_digests();
+	//task13();
+	ifstream enc_text_file("C:\\Users\\t440s\\source\\repos\\BOS3\\10\\14\\14data.txt.enc", ios::in | ios::binary);
+	ifstream key_file("C:\\Users\\t440s\\source\\repos\\BOS3\\10\\14\\14key.key", ios::in | ios::binary);
+	if (!enc_text_file) return 1;
+	if (!key_file) return 1;
+	string temp_string;
+	string enc_text;
+	//string dec_text;
+	string key_string;
+	AES_KEY key;
+	unsigned char key_buf[24];
+	uint8_t IV[24];
+	memset(IV,0, 24);
+
+	unsigned char dec_text[4000];
+
+	while (getline(enc_text_file, temp_string))
+	{
+		enc_text = enc_text + temp_string;
+	}
+	const int UserDataSize = (const int)enc_text.length();   // Get the length pre-padding
+	int RequiredPadding = (24 - (enc_text.length() % 24));   // Calculate required padding
+	std::vector<unsigned char> PaddedTxt(enc_text.begin(), enc_text.end());   // Easier to Pad as a vector
+	for (int i = 0; i < RequiredPadding; i++) {
+		PaddedTxt.push_back(0); //  Increase the size of the string by
+	}                           //  how much padding is necessary
+
+	unsigned char * UserData = &PaddedTxt[0];// Get the padded text as an unsigned char array
+	const int UserDataSizePadded = (const int)PaddedTxt.size();// and the length (OpenSSl is a C-API)
+
+
+	while (getline(key_file, temp_string))
+	{
+		key_string = key_string + temp_string;
+	}
+	//cout << enc_text << endl;
+	//cout << key_string << endl;
+	key_string = string_to_hex(key_string);
+	//cout << key_string << endl;
+	//DES_string_to_key(passwords[i].c_str(), &password_des);
+	hex2bin(key_string.c_str(), (char*)key_buf);
+	int j = 0;
+	unsigned char tmp_key_buf[24];
+	for (unsigned char i = 0; i < 0xFF; i++)
+	{
+		key_buf[23] = i;
+		memset(IV, 0, 24);
+		memcpy(tmp_key_buf, key_buf, 24);
+		//cout << key_buf << endl;
+		//string tmp_key = key_string;
+		//tmp_key += i;
+		//cout << tmp_key << endl;
+		j++;
+		cout << j << endl;
+		AES_set_decrypt_key(tmp_key_buf, 192, &key);
+		AES_cbc_encrypt((unsigned char*)enc_text.c_str(), dec_text, enc_text.length(), &key, IV, AES_DECRYPT);
+		//AES_cbc_encrypt((unsigned char*)text, dec_text, 50, &key, IV, AES_ENCRYPT);
+		//AES_cbc_encrypt(dec_text, dec_text, enc_text.length(), &key, IV, AES_DECRYPT);
+		//AES_cbc_encrypt(UserData, dec_text, UserDataSizePadded, &key, IV, AES_DECRYPT);
+		//AES_set_decrypt_key(key_buf, 192, &key);
+		//AES_decrypt(UserData, dec_text, &key);
+		//AesDecrypt(tmp_key, enc_text, dec_text);
+		//cout << endl;
+		//cout << i << endl;
+		cout << dec_text << endl;
+		//cout << endl;
+		
+	}
+	return 0;
+}
+int task13()
+{
 	const int len = 100;
 	const int strings = 100;
 	char ch = '\n';
@@ -74,13 +151,13 @@ int main()
 	tmp.close();
 	cout << enc_text << endl;
 
-	for (int i = 58; i < 59; i++) 
+	for (int i = 1; i < length; i++)
 	{
 		path = folder + str[i];
 		tmp.open(path);
 
 		//cout << path << endl;
-		memset(password_des,0,8);
+		memset(password_des, 0, 8);
 		if (!tmp) return 1;
 		while (getline(tmp, temp_string))
 		{
@@ -97,7 +174,7 @@ int main()
 		DES_set_key_checked((C_Block *)password_des, &keysched);
 
 		DES_cbc_encrypt((unsigned char*)enc_text.c_str(), dec_text, enc_text.length(), &keysched, &iv, DES_DECRYPT);
-		cout <<i<< endl;
+		cout << i << endl;
 		cout << dec_text << endl;
 		cout << endl;
 		//DES_cbc_encrypt((unsigned char*)folder.c_str(), dec_text, enc_text.length(), &keysched, &iv, 1);
@@ -109,9 +186,7 @@ int main()
 		tmp.close();
 	}
 	fs.close();
-	return 0;
 }
-
 string string_to_hex(const std::string& input)
 {
 	static const char* const lut = "0123456789ABCDEF";
@@ -150,3 +225,79 @@ void hex2bin(const char* src, char* target)
 	}
 }
 
+int AesDecrypt(const std::string &sAesKey,const std::string &ToDecrypt,std::string &Decrypted) 
+{
+	const int AesBlockSize = 16;
+	unsigned char cInitVector[AesBlockSize];
+	EVP_CIPHER_CTX oEncCtx;
+	const EVP_CIPHER *oChiper = 0;
+	int retval = 0;
+	unsigned char *cOutBuffer = 0;
+
+	// Set aes key: 
+	if (sAesKey.length() == 16) {
+		oChiper = EVP_aes_128_cbc();
+	}
+	else if (sAesKey.length() == 32) {
+		oChiper = EVP_aes_256_cbc();
+	}
+	else if (sAesKey.length() == 24) {
+		oChiper = EVP_aes_192_cbc();
+	}
+	else {
+		return -1; // Wrong key size. 
+	}
+
+	// Get init vector: 
+	const char *constInitVectorPtr = ToDecrypt.c_str();
+	for (int i = 0; i < AesBlockSize; i++) {
+		cInitVector[i] = *constInitVectorPtr;
+		constInitVectorPtr++;
+	}
+
+	// Create decryption context.	
+	EVP_CIPHER_CTX_init(&oEncCtx);
+	EVP_DecryptInit_ex(&oEncCtx, oChiper, 0, (unsigned char *)sAesKey.c_str(), cInitVector);
+
+	// Decrypt most of the data: 
+	int OutBufferByteLen = ToDecrypt.length() - AesBlockSize; // Subtract the InitVec. 
+	cOutBuffer = new unsigned char[OutBufferByteLen];
+	retval = EVP_DecryptUpdate(
+		&oEncCtx,
+		cOutBuffer,
+		&OutBufferByteLen,
+		((unsigned char *)ToDecrypt.c_str()) + AesBlockSize, // Remove the InitVector. 
+		OutBufferByteLen);
+
+	// Doese encryption fail? 
+	if (retval < 0) {
+		delete[] cOutBuffer;
+		return retval; // Encryption error. 
+	}
+
+	// Add encrypted data to output: 
+	Decrypted.append((char *)cOutBuffer, ((char *)cOutBuffer) + OutBufferByteLen);
+	delete[] cOutBuffer;
+
+	//Add last block+padding: 
+	OutBufferByteLen = 2 * AesBlockSize;
+	cOutBuffer = new unsigned char[OutBufferByteLen]; // To be sure add two blocks. 
+	retval = EVP_DecryptFinal_ex(
+		&oEncCtx,
+		cOutBuffer,
+		&OutBufferByteLen);
+
+	// Doese encryption fail? 
+	if (retval < 0) {
+		delete[] cOutBuffer;
+		return retval; // Encryption error. 
+	}
+
+	// Add encrypted data to output: 
+	Decrypted.append((char *)cOutBuffer, ((char *)cOutBuffer) + OutBufferByteLen);
+	delete[] cOutBuffer;
+	EVP_CIPHER_CTX_cleanup(&oEncCtx);
+	//if(OutBufferByteLen)
+	cout << OutBufferByteLen << endl;
+	return 0; // Success 
+}
